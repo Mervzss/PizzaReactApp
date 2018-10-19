96bf9a032f32226e3573a1f602dde750d5112c5d
Yondu Coders <bc00001mn@gmail.com>
2018-10-19 13:57:27 +0800

    last update

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ import {
   View,
 } from 'react-native';
 import PickerInputPart from './PizzaRelated/pickerInput';
-import CheckBocSelect from './PizzaRelated/checkboxSelect'
+import CheckBoxSelect from './PizzaRelated/checkboxSelect'
 // PIZZA ASSETS
 export default class App extends Component {
 
@@ -16,12 +16,15 @@ export default class App extends Component {
     super(props);
     this.state={
       size: null,
-      vegetableToppings:null,
-      nonVegetableToppings: null,
-      Cheese:null,
+      vegetableToppings:[],
+      nonVegetableToppings: [],
+      Cheese:[],
     }
   }
-
+  finaltransval=(name)=>{ 
+    // alert("ywYYW")
+    // this.state.vegetableToppings[0].checkState
+  }
 
   guard =()=> this.state.pizzaSize.map(name =>{
     return(
@@ -34,6 +37,43 @@ export default class App extends Component {
       })
     }
 
+  onaddVegetableToppings=(name,data,checkval)=>{
+    this.setState(prevState=>{
+      return{
+        vegetableToppings: prevState.vegetableToppings.concat({
+          toppings: name,
+          value:data,
+          checkState:checkval
+        })
+      }
+    })
+    var placeholder=0;
+    
+    
+  }
+  onaddNonVegetableToppings=(name,value)=>{
+    this.setState(prevState => {
+      return {
+        vegetableToppings: prevState.vegetableToppings.filter(toppings => {
+          return toppings.name !== name;
+          
+        })
+      };
+    });
+  }
+  onaddCheese=(name,value)=>{
+    
+  }
+  removeVegetableToppings=(name,value)=>{
+
+  }
+  removeNonVegetableToppings=(name,value)=>{
+  }
+  removeCheese=()=>{
+
+  }
+    
+
   render() {
     return (
       <View style={styles.container}>
@@ -42,7 +82,15 @@ export default class App extends Component {
         <Text style={styles.pickerText}>Size:</Text>
         <PickerInputPart sizeValue={this.addSizeValue} getsizeValue={this.state.size}/>
         </View>
-        <CheckBocSelect/>
+        <CheckBoxSelect
+        transval={this.finaltransval}
+        addVegetableToppings={this.onaddVegetableToppings}
+        addNonVegetableToppings={this.onaddNonVegetableToppings}
+        addCheese={this.onaddCheese}
+        removeVegetableToppings={this.onremoveVegetableToppings}
+        removeNonVegetableToppings={this.onremoveNonVegetableToppings}
+        removeCheese={this.onremoveCheese}
+        />
         
       </View>
     );
--- PizzaRelated/checkboxSelect.js
+++ PizzaRelated/checkboxSelect.js
@@ -3,36 +3,93 @@ import React,{Component} from 'react'
 import {CheckBox,View,Text,StyleSheet,SectionList} from 'react-native'
 
 import {pizzaBuilder} from '../PizzaAssets/PizzaAssets'
-
+import {VEGETABLE_TOPPINGS,NON_VEGETABLE_TOPPINGS,CHEESE} from '../actionVariables/actionVariables'
 class checkboxSelect extends Component{
 
-    // checkboxLoop =()=> pizzaBuilder.vegetable_toppings.map(val =>{
-    //     return(
-    //         <View style={styles.checkText}>
-    //             <CheckBox value={false}
-    //             testID="dsf"
-    //             />
-    //                 <Text style={{marginTop: 5}}> {val.toppings}</Text>
-    //         </View>
-    //     )
-    // })
+    state={
+        pizzaBuilder,
+
+    }
+
+    callCheckboxChange=(val,name,data,index,type)=>{
+        if(val){
+            switch(type)
+            {
+                case VEGETABLE_TOPPINGS:
+
+                //alert(this.state.vegetablecheckState[0])
+                this.props.addVegetableToppings(name,data,val)
+                break;
+                case NON_VEGETABLE_TOPPINGS:
+                this.setState({
+                    checkBoxState : state.non_vegetable_toppings[index].onCheckState = true
+                }
+                )
+                this.props.addNonVegetableToppings(name,data)
+                break;
+                case CHEESE:
+                this.setState({
+                    checkBoxState : state.cheese[index].onCheckState = true
+                }
+                )
+                this.props.addCheese(name,data)
+                break;
+                default:
+                return;
+            }
+            
+        }
+        else{
+            switch(type)
+            {
+                case VEGETABLE_TOPPINGS:
+                this.setState(prevState=>{
+                    return{
+                        pizzaBuilder: prevState.pizzaBuilder.vegetable_toppings.find(veggies =>{
+                            return veggies.onCheckState[index] = false
+                        })
+                    }
+                  })
+                this.props.removeVegetableToppings(name,data)
+                break;
+                case NON_VEGETABLE_TOPPINGS:
+                this.setState({
+                    checkBoxState : state.non_vegetable_toppings[index].onCheckState = false
+                }
+                    
+                )
+                this.props.removeNonVegetableToppings(name,data)
+                break;
+                case CHEESE:
+                this.setState({
+                    checkBoxState : state.cheese[index].onCheckState = false
+                } 
+                )
+                this.props.removeCheese(name,data)
+                break;
+                default:
+                return;
+            }
+        }
+    }
+
     sectionList =()=>{
         return(
             <SectionList
                 renderItem={({item, index, section}) => 
                  <View style={styles.checkText} key={index}>
-                    <CheckBox style={styles.checkBoxStyle} value={false} 
-                onValueChange={val=> this.callCheckboxChange(val,item.name,data,index,section.index)}
+                    <CheckBox style={styles.checkBoxStyle} value={this.gg} 
+                onValueChange={val=> this.callCheckboxChange(val,item.name,item.value,index,section.type)}
                 />
-                    <Text style={styles.textStyle}> {section.index}{item.name}</Text>
+                    <Text style={styles.textStyle}> {item.name}</Text>
             </View>}
   renderSectionHeader={({section: {title}}) => (
     <Text style={{fontWeight: 'bold',fontSize:16}}>{title}</Text>
   )}
   sections={[
-    {title: 'Vegetable Toppings', data: pizzaBuilder.vegetable_toppings,index:0},
-    {title: 'Non Vegetable Toppings', data: pizzaBuilder.non_vegetable_toppings,index:1},
-    {title: 'Cheese', data: pizzaBuilder.cheese,index:2},
+    {title: 'Vegetable Toppings', data: pizzaBuilder.vegetable_toppings,type:VEGETABLE_TOPPINGS},
+    {title: 'Non Vegetable Toppings', data: pizzaBuilder.non_vegetable_toppings,type:NON_VEGETABLE_TOPPINGS},
+    {title: 'Cheese', data: pizzaBuilder.cheese,type:CHEESE},
   ]}
   keyExtractor={(item, index) => item.name + index}
 />
